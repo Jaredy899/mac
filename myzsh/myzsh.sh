005fc050af95fd962d8c1a099cde46ab7dd12415
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-10-18 21:51:00 -0400

    Update myzsh.sh

--- myzsh/myzsh.sh
+++ myzsh/myzsh.sh
@@ -5,7 +5,7 @@ GITPATH="$(cd "$(dirname "$0")" && pwd)"
 echo "GITPATH is set to: $GITPATH"
 
 # GitHub URL base for the necessary configuration files
-GITHUB_BASE_URL="https://raw.githubusercontent.com/Jaredy899/mac/main/myzsh"
+GITHUB_BASE_URL="https://raw.githubusercontent.com/Jaredy899/mac/refs/heads/main/myzsh"
 
 # Function to install dependencies
 installDepend() {
